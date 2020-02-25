a331716bede99e21eba2062d747908fb89833f59
Neuromancer93 <itrejonumber1@gmail.com>
2020-02-24 22:31:18 -0800

    feat: hard coded scraping website, first successful test

--- lib/services/services.dart
+++ lib/services/services.dart
@@ -1,14 +1,20 @@
 import 'package:flutter_first_scraper/core/DI/IServices.dart';
 import 'package:flutter_first_scraper/core/models/scrapeModel.dart';
 import 'package:http/http.dart' as http;
+import 'package:html/parser.dart';
 
-const url = "https://www.pcso.gov.ph";
+const url = "https://flutter.dev/docs/reference/widgets";
+const resultId = 'card-title';
 
 class Services implements IServices {
   @override
   Future<List<ScrapeModel>> getLottoResults() async {
-    var doc = await http.get(url);
-    print(doc.body.toString());
+    var result = await http.get(url);
+    var doc = parse(result.body);
+    var element = doc.getElementsByClassName(resultId);
+    for (var i = 0; i < element.length; i++) {
+      print(element[i].text);
+    }
     return null;
   }
 }
